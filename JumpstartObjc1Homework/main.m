edecbfe9206c82b27a3cb9d19f6f7cfdf55e37b0
James Derry <jderry@derryitconsulting.com>
2014-02-01 11:39:36 -0600

    Initial Commit

---
+++ JumpstartObjc1Homework/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JumpstartObjc1Homework
+//
+//  Created by James Derry on 2/1/14.
+//  Copyright (c) 2014 James Derry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
